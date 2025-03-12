c6e3c7d4dd48489a620b9c64d20653c5e968ecd6
Priyakorn Rodwinit <pkr54939@gmail.com>
2025-03-12 13:59:05 +0700

    Update Scritp

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// Function to load view dynamically and replace buttons
-function loadView(view) { // view =>> province.html
+function loadView(view, district) {
     fetch(view)
         .then(response => response.text())
         .then(data => {
             document.getElementById("buttonsContainer").style.display = "none"; // Hide buttons
-            document.getElementById("content").innerHTML = data; // Show new view ==> province.html
+            document.getElementById("content").innerHTML = data; // Show new view
+
+            // Redirect with district parameter
+            if (district) {
+                window.location.href = `${view}?district=${encodeURIComponent(district)}`;
+            }
         })
         .catch(error => console.error("Error loading view:", error));
 }
+
